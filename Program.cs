24cec55e14173db764ecf2c126c6ede80966abbb
Büşra Göktaş <busra1gkts@gmail.com>
2024-02-11 21:34:10 +0300

    new c# ders

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using customer;
+
+IndividualCustomer customer1 = new IndividualCustomer();
+
+customer1.Id = 1;
+customer1.FirstName = "Büşra";
+
+IndividualCustomer customer2 = new IndividualCustomer();
+
+customer2.Id = 2;
+customer2.FirstName = "Özge";
+
+BaseCustomer[] customers = { customer1, customer2 };
+
+//polymorphism
+foreach (BaseCustomer customer in customers)
+{
+    Console.WriteLine(customer.Id);
+
+}
---
+++ customer/IndividualCustomer.cs
@@ -0,0 +1,21 @@
+﻿namespace customer;
+
+public class IndividualCustomer : BaseCustomer
+{
+
+    public string CustomerNumber { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+}
+
+public class CorporateCustomer : BaseCustomer
+{
+
+}
+
+public class BaseCustomer
+{
+    public int Id { get; set; }
+
+}
